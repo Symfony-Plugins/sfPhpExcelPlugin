6524c7e77fbc4e03f2932b57dcf141f258a49bdf
Garfield-fr <Garfield-fr@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-02-13 04:17:59 -0600

    initial commit
    git-svn-id: http://svn.symfony-project.com/plugins/sfPhpExcelPlugin/trunk@7473 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ examples/07reader.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * PHPExcel
+ *
+ * Copyright (C) 2006 - 2007 PHPExcel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category   PHPExcel
+ * @package    PHPExcel
+ * @copyright  Copyright (c) 2006 - 2007 PHPExcel (http://www.codeplex.com/PHPExcel)
+ * @license    http://www.gnu.org/licenses/lgpl.txt	LGPL
+ * @version    1.5.5, 2007-12-24
+ */
+
+/* Modified by Bertrand Zuchuat */
+require_once 'symfony.inc.php';
+
+$file_path = dirname(__FILE__) . '/05featuredemo.xlsx';
+
+if (!file_exists($file_path)) {
+	exit("Please run 05featuredemo.php first.\n");
+}
+
+echo date('H:i:s') . " Load from Excel2007 file\n";
+$objReader = new PHPExcel_Reader_Excel2007;
+$objPHPExcel = $objReader->load($file_path);
+
+echo date('H:i:s') . " Write to Excel2007 format\n";
+$objWriter = new PHPExcel_Writer_Excel2007($objPHPExcel);
+$objWriter->save(str_replace('.php', '.xlsx', __FILE__));
+
+
+echo date('H:i:s') . " Done writing files.\r\n";
---
+++ lib/sfPhpExcel.class.php
@@ -0,0 +1,6 @@
+<?php
+
+class sfPhpExcel extends PHPExcel
+{
+  
+}
