d5114fd1a134f8248363422ef5889dabd8268fc5
Garfield-fr <Garfield-fr@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-07-07 07:01:11 -0500

    Add Externals for Classes
    git-svn-id: http://svn.symfony-project.com/plugins/sfPhpExcelPlugin/trunk@10163 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- config/config.php
+++ config/config.php
@@ -1,3 +1,3 @@
 <?php
 
-ini_set('include_path', ini_get('include_path'). PATH_SEPARATOR .dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'lib');
+ini_set('include_path', ini_get('include_path'). PATH_SEPARATOR .dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'lib'. DIRECTORY_SEPARATOR .'PHPExcel');
